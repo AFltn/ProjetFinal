5f42bd9077abc388f1321ab66d4a24318883a0fa
Kyllian <kyllian.bourgade@gmail.com>
2021-06-28 16:19:52 +0200

    debut code class

--- projet-final-angular/src/app/model/ligne-commande.ts
+++ projet-final-angular/src/app/model/ligne-commande.ts
@@ -1,2 +1,38 @@
 export class LigneCommande {
+  constructor(
+    private _key: number | any = null,
+    private _quantite: number = 0
+  ) {}
+
+  /**
+   * Getter key
+   * @return {number }
+   */
+  public get key(): number {
+    return this._key;
+  }
+
+  /**
+   * Getter quantite
+   * @return {number }
+   */
+  public get quantite(): number {
+    return this._quantite;
+  }
+
+  /**
+   * Setter key
+   * @param {number } value
+   */
+  public set key(value: number) {
+    this._key = value;
+  }
+
+  /**
+   * Setter quantite
+   * @param {number } value
+   */
+  public set quantite(value: number) {
+    this._quantite = value;
+  }
 }
