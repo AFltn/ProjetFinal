474f16352318aea1c690ffc0231220d6000813b9
kylliaaan <83945945+kylliaaan@users.noreply.github.com>
2021-06-28 12:45:25 +0200

    Création

---
+++ projetFinal/src/main/java/sopraAjc/projetFinal/ProjetFinalApplication.java
@@ -0,0 +1,13 @@
+package sopraAjc.projetFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetFinalApplication.class, args);
+	}
+
+}
