0e9c55024514fc347d68cb55ba816cc16b7d4658
Kyllian <kyllian.bourgade@gmail.com>
2021-06-30 18:24:19 +0200

    push suivant

--- projet-final-angular/src/app/component/home/home.component.ts
+++ projet-final-angular/src/app/component/home/home.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { JeuxVideo } from 'src/app/model/jeux-video';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  jeuxVideos: Observable<JeuxVideo> | any = null;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
